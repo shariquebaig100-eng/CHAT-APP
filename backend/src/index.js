ca78f113063b2350487685d91bc60344bff09cdf
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-06-27 13:14:47 +0530

    Initial push or project update

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,13 +30,13 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"/frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "frontend", "dist");
+  app.use(express.static(frontendPath));
 
-
-  app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
 
 

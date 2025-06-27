4db4eb191b20d1d78fe2af8acbe24f456b51f102
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-06-27 18:06:20 +0530

    Initial push or project update

--- backend/src/routers/auth.route.js
+++ backend/src/routers/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, logout, signup, updateProfile,checkAuth } from '../controller/auth.controller.js';
+import { login, logout, signup, updateProfile, checkAuth } from '../controller/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();

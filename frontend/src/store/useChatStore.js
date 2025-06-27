d8ae9d532629230f7bcd9d1946e7656487d74825
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-06-27 17:22:49 +0530

    Initial push or project update

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -64,5 +64,5 @@ export const useChatStore = create((set, get) => ({
         const socket = useAuthStore.getState().socket;
         socket.off("NewMessage");
     },
-    
+    setSelectedUser: (selectedUser) => set({ selectedUser })
 }))

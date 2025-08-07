7c57a1227ad187061240e27db017112b7e115ff0
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-08-07 16:42:53 +0530

    update code

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import path from "path"
+// import path from "path"
 
 
 import authRoutes from './routers/auth.route.js';
@@ -17,30 +17,35 @@ dotenv.config()
 
 
 const PORT = process.env.PORT
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(cookieParser());
+const allowedOrigin =[
+  "http://localhost:5173","https://chat-app-1-xt2p.onrender.com"
+]
 
 app.use(express.json());//this wil allow to use json data out of the body
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: allowedOrigin,
   credentials: true,
 }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  })
-}
-
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   })
+// }
+app.get("/",(req,res)=>{
+  res.send("Backend is running...")
+});
 
 server.listen(PORT, () => {
-  console.log('Server is running on PORT :', + PORT);
+  console.log(`Server is running on PORT : ${PORT}`);
   connectDB();
 });

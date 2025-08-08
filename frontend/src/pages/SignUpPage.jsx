f1b1e38929ca64d5b121f695774d98edbfc957d8
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-08-08 10:55:24 +0530

    update code

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -42,7 +42,7 @@ export const SignUpPage = () => {
                             <div className='size-12 rounded-xl bg-primary/10 flex items-center justify-center 
                         group-hover:bg-primary/20 transition-colors'>
                                 <MessageSquare className='w-8  h-8 text-primary' />
-                                <h1>PChat</h1>
+                                
                             </div>
                             <h1 className='text-2xl font-bold mt-2'>Create Account</h1>
                             <p className='text-base-content/60'>Get started with your free account</p>

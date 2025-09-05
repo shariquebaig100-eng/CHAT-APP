31242ce9eb27f61a745c1328c98aab3292882297
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-09-05 17:32:51 +0530

    logo visible

---
+++ src/lib/utils.js
@@ -0,0 +1,9 @@
+import { Hourglass } from "lucide-react";
+
+export function formatMessageTime(date) {
+    return new Date(date).toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false
+    })
+}

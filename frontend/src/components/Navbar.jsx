b327d35e461bc171684b907e5885b0a9c8d1866c
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-09-05 18:41:02 +0530

    chore: cleaned ignored files from repo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,12 +10,12 @@ export const Navbar = () => {
     backdrop-blur-lg bg-base-100/80'>
        <div className='container mx-auto px-4 h-16'>
         <div className='flex items-center justify-between h-full'>
-          <div className='flex lg:-mx-10 gap-4'>
+          <div className='flex items-center gap-8'>
             <Link to="/" className='flex items-center gap-2.5 hover:opacity-80 transition-all'>
             <div className='size-9 rounded-lg bg-primary/10 flex items-center justify-center'>
-            <MessageSquare className='w-10  h-7 text-primary' />
+            <MessageSquare className='w-5  h-5 text-primary' />
             </div>
-            <h1 className='text-lg fnt-bold'>PChat </h1>
+            <h1 className='text-lg font-bold'>PChat </h1>
             </Link>
           </div>
           <div className='flex items-center gap-2'>

264b10efc985f04e06e4f70814af60f822c252b6
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-09-09 20:07:55 +0530

    backend ready with previus code

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: ["http://localhost:5173"],
-        origin:["https://chat-lwcap7yqt-sharique-baigs-projects.vercel.app"]
+        origin:["chat-app-phi-ashen-92.vercel.app"]
     },
 });
 

dadd513c371dc7cabdc462eefbffa076c3928927
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-09-09 18:52:48 +0530

    backend ready with mobie

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { Server } from "socket.io";
 import http from "http";
-import express from "express";
 
 const app = express();
 const server = http.createServer(app);

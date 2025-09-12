d9a91ca08352b359e21602d97f71e85e4e5e83d5
shariquebaig100-eng <shariquebaig100@gmail.com>
2025-09-12 12:17:18 +0530

    Updated with None

--- backend/src/lib/utlis.js
+++ backend/src/lib/utlis.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         httpOnly: true,
-        sameSite: "none",
+        sameSite: "None",
         secure: true, // Use secure cookies in production
         path:"/",
     }
